908fd78145deb1fd8aa43af580bd3af53ae00700
Filip Laszczak <68005942+EXCV31@users.noreply.github.com>
2020-12-04 13:36:24 +0100

    Update add to filename.py

--- add to filename.py
+++ add to filename.py
@@ -10,3 +10,22 @@ for allx in os.listdir(directory):
     os.rename(directory + "/" + allx, directory + "/" + name_list[0] + "_MCH_MT" + name_list[1])
 
 root.destroy()
+
+
+# optional, if you have separator in file name:
+
+"""
+from tkinter import filedialog
+from tkinter import Tk
+import os
+root = Tk()
+root.withdraw()
+directory = filedialog.askdirectory()
+
+for allx in os.listdir(directory):
+    file_name = str.split(allx, "-") # replace "-" with "separator"
+    os.rename(directory + "/" + allx, directory + "/" + file_name[0] + "_MCH_MT.nxf")
+
+root.destroy()
+
+"""
