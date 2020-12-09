d37aaaf63b1c12c451185328d3534d9959d489d7
Filip Laszczak <68005942+EXCV31@users.noreply.github.com>
2020-12-09 19:42:10 +0100

    Update NX run and wait for exit + drag to right corner.py

--- NX run and wait for exit + drag to right corner.py
+++ NX run and wait for exit + drag to right corner.py
@@ -12,12 +12,21 @@ directory = filedialog.askdirectory()
 
 for allx in os.listdir(directory):
     process = subprocess.Popen(["C:\\Program Files (x86)\\Dynalab\\NX\\NXEditor.exe", directory + "/" + allx])
-    time.sleep(1.5)
-    pyautogui.moveTo(963, 260)
+    time.sleep(1.2)
+    pyautogui.moveTo(360, 20) # program x y
     time.sleep(0.2)
     pyautogui.mouseDown()
-    pyautogui.moveTo(1366, 360)
+    pyautogui.moveTo(1366, 360) # right pane
     pyautogui.mouseUp()
+    time.sleep(0.2)
+    pyautogui.moveTo(950, 40) # combobox
+    pyautogui.mouseDown()
+    pyautogui.mouseUp()
+    time.sleep(0.2)
+    pyautogui.moveTo(920, 100) # workflow
+    pyautogui.mouseDown()
+    pyautogui.mouseUp()
+
     process.wait()
 
 ctypes.windll.user32.MessageBoxW (0, 'OK', "Jest super", 0x10)
